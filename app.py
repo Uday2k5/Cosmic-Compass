58a36ed13ce71d7d5f1483c4572f733294727b0f
Uday Kumar <166249239+Uday2k5@users.noreply.github.com>
2026-01-22 21:59:33 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -3,14 +3,24 @@ import tensorflow as tf
 import pickle
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os # Add this import
+
 
 app = Flask(__name__)
 CORS(app)  
 
+
+# Get the directory where app.py is located
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 try:
-    model = tf.keras.models.load_model('asteroid_model.h5')
+    # Use os.path.join to create the full path
+    model_path = os.path.join(BASE_DIR, 'asteroid_model.h5')
+    scaler_path = os.path.join(BASE_DIR, 'data_scaler.pkl')
     
-    with open('data_scaler.pkl', 'rb') as f:
+    model = tf.keras.models.load_model(model_path)
+    
+    with open(scaler_path, 'rb') as f:
         scaler = pickle.load(f)
         
     print("Model and scaler loaded successfully.")
@@ -18,6 +28,18 @@ except Exception as e:
     print(f"Error loading model or scaler: {e}")
     model = None
     scaler = None
+
+# try:
+#     model = tf.keras.models.load_model('asteroid_model.h5')
+    
+#     with open('data_scaler.pkl', 'rb') as f:
+#         scaler = pickle.load(f)
+        
+#     print("Model and scaler loaded successfully.")
+# except Exception as e:
+#     print(f"Error loading model or scaler: {e}")
+#     model = None
+#     scaler = None
 
 INPUT_LENGTH = 60
 OUTPUT_LENGTH = 10
